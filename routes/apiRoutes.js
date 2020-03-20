c5d78017b63b9b160717a744d97d015c20db9600
Paul Han <paul.myung.han@gmail.com>
2020-03-19 18:36:29 -0700

    set up additional html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// get route
+router.get()
+
+// post route
+
+
+// delete route
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
 const path = require('path');
 const app = express();
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoutes.js");
 
 // If we are in production (Heroku), process.env.PORT is true, 
 // If we are in development it is false, default to 3000
@@ -9,16 +10,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
-})
-
-
 app.use(express.static('public'));
+
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
 
 app.listen(PORT);
