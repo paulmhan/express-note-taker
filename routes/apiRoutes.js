d06b3f15a255c191ff80b1f08ee26dddd420e84b
Paul Han <paul.myung.han@gmail.com>
2020-03-20 15:58:50 -0700

    created get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
-// get route
-router.get()
+const db = require("../db/db.json");
+const fs = require("fs");
+
+router.get("/notes", (req,res) => { 
+    fs.readFileSync(db ,function (err, data){
+        if (err){
+            console.log(err);
+        }
+        res.json(data);
+    })
+})
 
 // post route
+router.post("/notes", (req,res) => {
+    let newNote = req.body.data;
 
+//use fs to save notes to db.json
+    res.json()
+})
 
 // delete route
+router.delete("/notes/:id", (req,res) => {
+    //grab id from req.params
+    let noteId = req.params.id;
+    fs.unlink()
+    //use fs to delete the note
+    res.json()
+})
 
 
 
