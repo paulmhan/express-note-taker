799007f1ec8d38e04866b5a378042e05516b04d1
Paul Han <paul.myung.han@gmail.com>
2020-03-23 17:38:49 -0700

    delete route and added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,32 +3,35 @@ const db = "routes/../db/db.json";
 const fs = require("fs");
 
 //get route
+//when url hits /notes, get the data from the db.json file and send it back
 router.get("/notes", (req, res) => {
     fs.readFile(db, "utf8", function (err, data) {
         if (err) {
             console.log(err);
         }
+        //JSON always returns string, so parse it to make it an object
         let dbData = JSON.parse(data);
+        //send data back 
         res.json(dbData);
     })
 })
 
 // post route
 router.post("/notes", (req, res) => {
-    console.log(req.body);
+    //req.body only when posting, undefined until we use body parser
     let newNote = req.body;
-    fs.readFile(db, "utf8",function (err, data) {
+    fs.readFile(db, "utf8", function (err, data) {
         if (err) {
             console.log(err);
-        } 
+        }
         let json = JSON.parse(data);
         newNote.id = json.length + 1;
         json.push(newNote);
-        fs.writeFile(db, JSON.stringify(json), function (err){
-            if(err){
+        fs.writeFile(db, JSON.stringify(json), function (err) {
+            if (err) {
                 console.log(err);
             }
-            res.json({success:true});
+            res.json({ success: true });
         })
     })
 })
@@ -37,12 +40,28 @@ router.post("/notes", (req, res) => {
 router.delete("/notes/:id", (req, res) => {
     //grab id from req.params
     let noteId = req.params.id;
-    fs.unlink()
-    //use fs to delete the note
-    res.json()
+    fs.readFile(db, "utf8", (err, data) => {
+        if (err) {
+            console.log(err)
+        }
+        const currentNotes = JSON.parse(data);
+        const newNotes = currentNotes.filter(note => note.id != noteId);
+        fs.writeFile(db, JSON.stringify(newNotes), function (err) {
+            if (err) {
+                console.log(err);
+            }
+            res.json(db);
+        })
+    })
 })
 
 
 
 
+
 module.exports = router;
+
+
+//Musa:
+//req.body
+//line34
